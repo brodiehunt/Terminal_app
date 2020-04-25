ecbbe426ce4cd64a2fd6cc53661ee3dd3db81297
Brodie Hunt <brodiehunt7@gmail.com>
2020-04-25 16:57:43 +1000

    Game stages added

--- src/game.rb
+++ src/game.rb
@@ -1,8 +1,32 @@
 class Game 
+  attr_accessor :game_stage
   def initialize
+    @score = 0 
+    @game_stage = 'start'
   end
 
   def draw
+    change_text_display
+  end
+
+  def change_text_display
+    if @game_stage == 'start'
+      Image.new('./../media/trump_background.png', x: 110, y: 50, width: 600, height: 400)
+      Text.new("WELCOME! PRESS 'P' TO PLAY", x: 60, y: 450, size: 45, color: 'red')
+      Text.new("'W'=UP, 'S'=DOWN, 'A'=LEFT, 'D'=RIGHT", x: 20, y:500, size: 40, color: 'blue')
+    elsif @game_stage == 'play'
+      Text.new("Score: #{@score}", x: 10, y: 560, color: 'white', size: 30)
+    else 
+      Image.new('./../media/trump_pepe.png', x: 110, y:50, width: 600, height: 400)
+      Text.new("YOU DIED!!!! YOUR SCORE IS #{@score}", x: 20, y: 450, color: 'red', size: 40)
+      Text.new("PRESS 'R' TO RESTART, 'T' TO QUIT", x: 20, y: 500, color: 'blue', size: 45)
+    end
   end
   
+  def score_increase
+    @score += 10
+  end
 end
+
+  
+
--- src/main.rb
+++ src/main.rb
@@ -10,19 +10,33 @@ HEIGHT = Window.height / GRID_SIZE
 game = Game.new 
 trump = Trump.new
 coin = Coin.new
+song = Music.new('./../media/supermario.mp3')
+fantastic_sample = Sound.new('./../media/fantastic.wav')
+dream_dead_sample = Sound.new('./../media/dream_dead.wav')
+song.play
 
 update do 
   clear
   game.draw
-  unless trump.hit_itself
-    trump.move_trump
-    trump.check_borders
-    coin.draw
-    trump.draw
-  end
-  if coin.trump_collect?(trump.collision_x, trump.collision_y)
-    coin.change_location
-    trump.increase_size
+  close if game.game_stage == 'quit'
+  if game.game_stage == 'play'
+    unless trump.hit_itself
+      trump.move_trump
+      trump.check_borders
+      coin.draw
+      trump.draw
+    end
+    if trump.hit_itself
+      game.game_stage = 'pause'
+      song.pause
+      dream_dead_sample.play
+    end
+    if coin.trump_collect?(trump.collision_x, trump.collision_y)
+      coin.change_location
+      trump.increase_size
+      game.score_increase
+      fantastic_sample.play
+    end
   end
 end
 
@@ -36,9 +50,17 @@ on :key_down do |event|
     trump.direction=(event.key) unless trump.direction == 's'
   when 's'
     trump.direction=(event.key) unless trump.direction == 'w'
+  when 'r'
+    trump = Trump.new
+    coin = Coin.new
+    game = Game.new
+    song.play
+  when 'p'
+    game.game_stage= 'play'
+  when 't'
+    game.game_stage= 'quit'
   end
 end
-
 show 
 
 
